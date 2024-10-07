6baaa5cd66b9fbb10b9a67d62dcaf800a4f097a2
pjparr <pjparr@gmail.com>
2024-10-07 13:49:50 +0800

    test app change

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ import streamlit as st
 import pandas as pd
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40] 
+  'second column': [10, 20, 30, 4099999] 
 }) 
 
 df
